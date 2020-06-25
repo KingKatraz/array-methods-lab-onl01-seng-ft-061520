3b462a8101dbd2fc35653f04fef0c43f8a1fe88b
Hal H Nader <halnader89@gmail.com>
2020-06-25 20:22:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def using_include(array, element)
   
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-    element1 = "wow"
-    element2 = "Tardis"
+    #element1 = "wow"
+    #element2 = "Tardis"
+    
 
 end
 
